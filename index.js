8d723f14a014634c00149302d3b72ac0b5b47876
Pritiranjan Mishra <mishrapritiranjan3@gmail.com>
2021-06-27 12:23:12 +0530

    putting it all togethor . added two reducers

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Library Code
 function createStore (reducer) {
     // The store should have four parts
     // 1. The state
@@ -32,9 +31,44 @@ function createStore (reducer) {
   
   // App Code
   function todos (state = [], action) {
-    if (action.type === 'ADD_TODO') {
-      return state.concat([action.todo])
-    }
+    switch(action.type) {
+        case 'ADD_TODO' :
+          return state.concat([action.todo])
+        case 'REMOVE_TODO' :
+          return state.filter((todo) => todo.id !== action.id)
+        case 'TOGGLE_TODO' :
+          return state.map((todo) => todo.id !== action.id ? todo :
+            Object.assign({}, todo, { complete: !todo.complete }))
+        default :
+          return state
+      }
   
-    return state
   }
+
+  function goals(state = [], action){
+      switch (action.type) {
+          case 'ADD_GOAL':
+              return state.concat([action.goal])
+              break;
+          case "REMOVE_GOAL":
+                return state.filter((goal) => goal.id !== action.id) 
+                break;
+          default:
+              return state;
+      }
+  }
+  
+  const store = createStore(todos)
+  
+  store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+  })
+  
+  store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+      id: 0,
+      name: 'Learn Redux',
+      complete: false
+    }
+  })
